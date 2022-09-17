8b53bf295ccf61f8737aa94be0ee374dfb26dd78
jmmon <jmmon93@gmail.com>
2022-09-17 16:28:30 -0400

    rework details to be like the itemPreview; rework controls

--- qwik-marketplace/src/components/details/details.tsx
+++ qwik-marketplace/src/components/details/details.tsx
@@ -1,104 +1,157 @@
-import {$, component$, Resource, useContext, useResource$, useStore, useStylesScoped$, useWatch$} from "@builder.io/qwik";
-import { SessionContext } from "~/libs/context";
-import { connect, fetchItemDataFromIPFS, getContract, getItem } from "~/libs/ethUtils";
+import {
+	$,
+	component$,
+	Resource,
+	useContext,
+	useResource$,
+	useStore,
+	useStylesScoped$,
+	useWatch$,
+} from "@builder.io/qwik";
+import {SessionContext} from "~/libs/context";
+import {
+	connect,
+	fetchItemDataFromIPFS,
+	getContract,
+	getItem,
+	getItemsFromAddress,
+	onPurchase,
+} from "~/libs/ethUtils";
+import {shortText} from "~/libs/utils";
 import Styles from "./details.css";
 
-export default component$(({session}) => {
+export default component$((props: {item: IContractItem | null}) => {
+	const session = useContext(SessionContext);
 	useStylesScoped$(Styles);
-	// const item = session.details.item;
 
-	const handleClose = $(() => {
-		session.details.show = false;
-		console.log('closing details');
+	const itemDetailsResource = useResource$(
+		async ({track, cleanup}): Promise<IItemData | object> => {
+			// const testItem = track(session, "details.item");
+			// console.log({testItem});
+			const details = track(session, "details");
+			console.log({details});
+			if (!session.isBrowser) return Promise.resolve({});
+			if (!session.details.item) return Promise.resolve({});
+
+			const controller = new AbortController();
+			cleanup(() => controller.abort());
+
+			const item = await getItem(session.details.item.id);
+			return await fetchItemDataFromIPFS(item, controller);
+		}
+	);
+	
+	const handleClose$ = $(() => {
+		console.log("closing details");
+		session.details = {
+			...session.details,
+			show: false,
+		}
 	});
-
-	useWatch$(({track}) => {
-		const details = track(session, 'details');
-		if (details.show) {console.log('details watch:', session.details.item ?? 'no item yet');}
-		else console.log('HIDING details watch:', session.details.item ?? 'no item yet');
-	});
-
-	const itemDetailsResource = useResource$<IItemData>(async ({track, cleanup}) => {
-		track(session, "isBrowser");
-		if (!session.isBrowser) return Promise.resolve({});
-		
-		const controller = new AbortController();
-		cleanup(() => controller.abort());
-		
-		const item = await getItem(session.details.item.id, controller);
-		return fetchItemDataFromIPFS(item);
-	});
-
-
 
 	return (
-		<aside
-			class={`details wrapper ${
-				session.details.show && "showing"
-			}`}
-		>
-			<div class="details spacer"></div>
-			<div class={`details body ${
-				session.details.show && "showing"
-				}`}>
-				<button onClick$={handleClose} class="bg-blue-200 p-4 text-lg">
-					X
-				</button>
-				<div
-					class="flex flex-col w-full items-stretch"
-				>
-					<h1 class="mx-auto text-lg py-4">Details</h1>
+		<aside class={`details wrapper ${session.details.show && "showing"}`}>
+			<div class={`details body ${session.details.show && "showing"}`}>
+					<div class="flex bg-blue-100">
+						<button
+							onClick$={handleClose$}
+							class="bg-blue-200 p-4 text-lg w-[60px] h-[60px]"
+						>
+							X
+						</button>
+						<h1 class="mx-auto text-lg py-4 pr-[60px]">Details</h1>
+					</div>
 					<Resource
 						value={itemDetailsResource}
 						onPending={() => <div>Loading...</div>}
 						onRejected={(error) => <div>Error: {error.message}</div>}
-						onResolved={(itemData) => {
-							console.log("parent resolve", {itemData});
-							return (Object.keys(itemData).length > 0) 
-								? <ItemDetails itemData={itemData} />
-								: <div>Loading...</div>;
-						}}
+						onResolved={(itemData) =>
+							Object.keys(itemData).length > 0 ? (
+								<ItemDetails itemData={itemData} />
+							) : (
+								<div>Loading...</div>
+							)
+						}
 					/>
-					
 				</div>
-			</div>
-			<div class="details spacer"></div>
 		</aside>
 	);
 });
 
+export const ItemDetails = component$(({itemData}: {itemData: IItemData | any}) => {
+	const session = useContext(SessionContext);
+	useStylesScoped$(`.detailsWrapper {
+		grid-template-rows: 60px 40% auto;
+		overflow-y: auto;
+	}`);
 
+	const seeStore$ = $(async (address: string) => {
+		console.log('seeStore: opening store for ', address);
 
-export const ItemDetails = component$(({itemData} : {itemData: IItemData}) => {
-	const onPurchase = $(async (price) => {
-		const address = await connect();
-		const contract = await getContract(true);
-		
-		const options = {value: `${price}`}
-		const tx = await contract.sell(itemData.id, options);
-		console.log('response from purchase:', {tx});
-	})
-
-
+		session.details = {
+			...session.details,
+			show: false,
+		}
+	
+		session.store = {
+			show: true, 
+			address,
+			items: await getItemsFromAddress(address),
+		}
+	});
 
 	return (
-		<div class="w-full p-4 h-[800px] bg-slate-300">
+		<div class="detailsWrapper w-full p-4 bg-gray-100 grid">
+			<h1 class="text-4xl text-center text-gray-700 p-2">{itemData.name}</h1>
 			<div
-				style={`background: url(${itemData.imgUrl}); background-repeat: no-repeat; background-size: cover; background-position: center; height: 50%; width: 100%;`}
+				style={`background: url(${itemData.imgUrl}); background-repeat: no-repeat; background-size: cover; background-position: center; height: 400px; width: 100%;`}
 			></div>
-			<h1>Item Name: {itemData.name}</h1>
-			<p>Price (in wei): {itemData.price}</p>
-			<p>Description: {itemData.description}</p>
-			{window.ethereum && <p>Owner: {itemData.owner}</p>}
-			<button
-				class="border-solid rounded px-2 py-1 bg-gray-50"
-				onClick$={async () => {
-					// console.log("TODO: Purchase", params.id);
-					await onPurchase(itemData.price);
-				}}
-			>
-				Purchase
-			</button>
+
+			<div class="flex flex-wrap gap-1 bg-gray-100 text-gray-700 p-2 flex-grow w-full">
+				<div class="flex-grow bg-gray-200 p-2">
+					<span class="text-sm text-gray-500">Name:</span>
+					<br />
+					<span class="ml-2 text-md">{itemData.name}</span>
+				</div>
+				<button
+					class="grow-0 w-3/12 border rounded bg-white mt-1 p-1 "
+					onClick$={async () => {
+						await onPurchase(itemData);
+					}}
+				>
+					Purchase
+				</button>
+				<div class="w-full bg-gray-200 p-2">
+					<span class="text-sm text-gray-500">Price:</span>
+					<br />
+					<span class="ml-2 text-md">{itemData.price} wei</span>
+				</div>
+				<div class="w-full bg-gray-200 p-2">
+					<span class="text-sm text-gray-500">Owner's Address:</span>
+					<br />
+					{session.address ? (
+						<span
+							class="text-blue-400 cursor-pointer text-md"
+							onClick$={() => seeStore$(itemData.owner)}
+						>
+							{itemData.owner}
+						</span>
+					) : (
+						<span class="text-md">{"#".repeat(42)}</span>
+					)}
+				</div>
+				<div class="w-full bg-gray-200 p-2">
+					<span class="text-sm text-gray-500">Description:</span>
+					<br/>
+					<span class=" text-md ml-2 border-solid rounded border-gray-600 p-2 break-all overflow-y-scroll z-50">
+						{itemData.description}{"#".repeat(9999)}
+					</span>
+				</div>
+				{session.address && (<div
+				class="text-blue-400 cursor-pointer text-sm"
+				onClick$={() => seeStore$(itemData.owner)}
+				>See more from this address</div>)}
+			</div>
 		</div>
 	);
 });
