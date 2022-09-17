9e4c2a9c75d84b7dcedcd38f5bc434320959d1d2
jmmon <jmmon93@gmail.com>
2022-09-17 16:18:31 -0400

    made 'connect' header a fixed modal

---
+++ qwik-marketplace/src/components/connect/connect.tsx
@@ -0,0 +1,16 @@
+import {component$, useContext, useStylesScoped$} from "@builder.io/qwik";
+import { SessionContext } from "~/libs/context";
+import { handleConnect } from "~/libs/ethUtils";
+import Styles from "./connect.css";
+
+export default component$(() => {
+  const session = useContext(SessionContext);
+	useStylesScoped$(Styles);
+
+	return (
+		<div class={`connector ${!session.address && "showing"}`}>
+			<button onClick$={() => handleConnect(session)}>Connect Metamask</button>
+			<div class="text">Connect With Your Metamask Wallet To Buy Items</div>
+		</div>
+	);
+});
--- qwik-marketplace/src/components/header/header.tsx
+++ qwik-marketplace/src/components/header/header.tsx
@@ -2,21 +2,15 @@ import {component$, useContext, useStyles$} from "@builder.io/qwik";
 import styles from "./header.css?inline";
 
 import {SessionContext} from "~/libs/context";
+import { handleConnect } from "~/libs/ethUtils";
 
-export default component$(({connect$}) => {
+export default component$(() => {
 	useStyles$(styles);
-  const session = useContext(SessionContext);
 
 	return (
 		<header>
 			<div class="header-inner">
 				<h1>Marketplace</h1>
-				<div class={`connector ${!session.address && 'showing'}`}>
-					<button onClick$={connect$}>
-						Connect Metamask
-					</button>
-					<div class="text">Connect With Your Metamask Wallet To Buy Items</div>
-				</div>
 			</div>
 		</header>
 	);
