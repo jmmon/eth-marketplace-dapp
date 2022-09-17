c191238f2272230f6fed0799765f004f1f2e4f01
jmmon <jmmon93@gmail.com>
2022-09-17 16:25:11 -0400

    move eth/metamask utils to ethUtils

--- qwik-marketplace/src/routes/layout.tsx
+++ qwik-marketplace/src/routes/layout.tsx
@@ -1,17 +1,17 @@
+declare var window: any;
 import {
 	component$,
 	Slot,
 	useClientEffect$,
 	useContextProvider,
 	useStore,
-	$,
 } from "@builder.io/qwik";
 import {RequestHandler, useEndpoint} from "@builder.io/qwik-city";
 import Footer from "../components/footer/footer";
 import Header from "../components/header/header";
 import { SessionContext } from "~/libs/context";
-import { connect, getContract } from "~/libs/ethUtils";
 import { Notifications } from "~/components/notifications/notifications";
+import Connect from "~/components/connect/connect";
 
 
 //display metamask login
@@ -29,58 +29,20 @@ export default component$(() => {
 			show: false,
 			item: null,
 		},
+		store: {
+			show: false,
+			address: '',
+			items: [],
+		},
 		notifications: {
 			each: [],
 			nextIndex: 0,
 		},
-		test: 0
 	} as ISessionContext,
 	{recursive: true});
 
 	useContextProvider(SessionContext, session);
 
-	const startWatchAddress = $(async () => {
-		session.address = (await window.ethereum.request({ method: 'eth_accounts' }))[0];
-		console.log('address:', session.address);
-		const accountInterval = setInterval(async () => {
-			console.log('account interval');
-			const address = (await window.ethereum.request({ method: 'eth_accounts' }))[0];
-
-			if (address !== session.address && session.address !== "undefined") {
-				console.log('new address:', session.address);
-				session.address = address;
-				// location.reload();
-			}
-
-			if (!address) {
-				if (session.address !== undefined) {
-					console.log('no address now, clearing address from context');
-				}
-				session.address = undefined;
-			}
-		}, 1000);
-
-		return () => clearInterval(accountInterval);
-	});
-
-	const handleConnect$ = $(async () => {
-		try {
-			// check if unlocked
-			console.log('checking if unlocked from handleConnect');
-			const address = (await window.ethereum.request({
-				method: "eth_requestAccounts",
-			}))[0];
-
-			//save address to our store
-			session.unlocked = true;
-			session.address = address;
-
-			startWatchAddress();
-	
-		} catch (error) {
-			console.log('Error connecting metamask:', error.message);
-		}
-	});
 	
 	useClientEffect$(async () => {
 		session.isBrowser = true;
@@ -92,8 +54,8 @@ export default component$(() => {
 
 	return (
 		<div>
-			<Header connect$={handleConnect$} />
-			{/* <div>{session.test}</div> */}
+			<Header />
+			<Connect />
 			<main>
 				<Slot />
 			</main>
