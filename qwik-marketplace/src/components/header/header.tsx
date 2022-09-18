6009bb22b903742a1290e26cde43b06005a5de96
jmmon <jmmon93@gmail.com>
2022-09-17 20:09:49 -0400

    header shows abbreviated address when connected

--- qwik-marketplace/src/components/header/header.tsx
+++ qwik-marketplace/src/components/header/header.tsx
@@ -2,15 +2,26 @@ import {component$, useContext, useStyles$} from "@builder.io/qwik";
 import styles from "./header.css?inline";
 
 import {SessionContext} from "~/libs/context";
-import { handleConnect } from "~/libs/ethUtils";
+import {handleConnect} from "~/libs/ethUtils";
 
 export default component$(() => {
+	const session = useContext(SessionContext);
 	useStyles$(styles);
 
 	return (
 		<header>
 			<div class="header-inner">
 				<h1>Marketplace</h1>
+				{session.address && (
+					<div class="text-red-200 justify-end pt-2">
+						Welcome,{" "}
+						{String.prototype.concat(
+							session.address.slice(0, 5),
+							"...",
+							session.address.slice(-4)
+						)}
+					</div>
+				)}
 			</div>
 		</header>
 	);
