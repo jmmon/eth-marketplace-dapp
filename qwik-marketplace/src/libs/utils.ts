364ccd2afa2f036a443ac476248acf7449160352
jmmon <jmmon93@gmail.com>
2022-09-25 07:59:57 -0400

    swapped couple lines

--- qwik-marketplace/src/libs/utils.ts
+++ qwik-marketplace/src/libs/utils.ts
@@ -10,12 +10,11 @@ export const shortAddress = (address: string) =>
   String.prototype.concat(address.slice(0, 5), "...", address.slice(-4));
 
 export const seeStore = (address: string, session: ISessionContext) => {
-  session.details.show = false;
-
   // filter our items to find all created by a given address
   const items = session.items.list.filter((item) => item?.owner === address);
-
-
+  
+  // hide details page, and set and show store page
+  session.details.show = false;
 	session.store = {
     items, 
     address, 
