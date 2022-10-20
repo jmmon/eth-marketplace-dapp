d33102f365aa06a612585f4424af9e87207feead
jmmon <jmmon93@gmail.com>
2022-10-20 18:00:24 -0400

    url swapped

--- qwik-marketplace/src/libs/ipfs.ts
+++ qwik-marketplace/src/libs/ipfs.ts
@@ -3,8 +3,8 @@ import { IPFS_POST_URL } from "~/libs/constants";
 import {create} from "ipfs-http-client";
 
 
-// export const IPFS_FETCH_URL = 'http://localhost:8080/ipfs/';
-export const IPFS_FETCH_URL = 'https://ipfs.io/ipfs/';
+export const IPFS_FETCH_URL = 'http://localhost:8080/ipfs/';
+// export const IPFS_FETCH_URL = 'https://ipfs.io/ipfs/';
 
 export const ipfsOptions = {
 	url: "http://127.0.0.1:5001",
